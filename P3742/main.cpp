2ea3beaa8c50b5fd32ba2fc5d26f17d5f95c674f
R1turn0 <714669315@qq.com>
2022-07-04 23:44:56 +0800

    updata

---
+++ P3742/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+int n;
+
+std::basic_string<char> funtion (std::string s1, std::string s2){
+    for (int i = 0; i < n; i++) {
+        if (s1[i] < s2[i]) {
+            return "-1";
+        }
+    }
+    return s2;
+}
+
+int main() {
+    std::string x, z, y;
+
+    std::cin >> n;
+    std::cin >> x >> z;
+
+    std::cout << funtion(x, z);
+
+    return 0;
+}
+
+// 这题比较有有意思的地方就是有多个解。
+//
+//如果X有任何一个地方小于Z，就找不到解，输出-1，结束。
+//
+//如果没有，因为Y本身就是Z的一种解，就不用找了，直接输出Z。
