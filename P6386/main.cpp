825047fd56c59f78e3ec4c3e7b8afbc808b6b207
R1turn0 <714669315@qq.com>
2022-09-15 23:08:13 +0800

    Upload

--- P6386/main.cpp
+++ P6386/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int a, b, c, d, p[10];
+
+string check(int x) {
+    if (0 < x % (a + b) && x % (a + b) <= a && 0 < x % (c + d) && x % (c + d) <= c) {
+        return "both";
+    }
+    if((x % (a + b) > a || x % (a + b) == 0) && (x % (c + d) > c || x % (c + d) == 0)) {
+        return "none";
+    }
+    else{
+        return "one";
+    }
+}
+
+int main(){
+    cin >> a >> b >> c >> d >> p[1] >> p[2] >> p[3];
+    for (int i = 1; i <= 3; ++i){
+        cout << check(p[i]) << endl;
+    }
     return 0;
 }
