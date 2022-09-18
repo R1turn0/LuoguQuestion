d9c49bb3a147f728d1c4c4260f7063fd432eb26a
R1turn0 <714669315@qq.com>
2022-09-18 18:07:08 +0800

    Upload

---
+++ P5705/P5705/P5705.cpp
@@ -0,0 +1,18 @@
+﻿// P5705.cpp: 定义应用程序的入口点。
+//
+
+#include "P5705.h"
+
+int main()
+{
+	std::string str;
+
+	std::cin >> str;
+
+	for (int i = (int)str.length() - 1; i >= 0; i--)
+	{
+		std::cout << str[i];
+	}
+
+	return 0;
+}
--- P5705/cmake-build-debug/cmake_install.cmake
+++ P5705/cmake-build-debug/cmake_install.cmake
@@ -42,6 +42,12 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/r1turn0/WorkSpace/Cpp/Luogu/P5705/cmake-build-debug/P5705/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
