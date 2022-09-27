cfd61b49547a04fa33530a6e2b480135cdec9a18
R1turn0 <74232749+R1turn0@users.noreply.github.com>
2022-09-27 21:21:37 +0800

    Add files via upload

---
+++ DoubleArrayAverageProjectC/main.c
@@ -0,0 +1,27 @@
+#include "DoubleArrayAverage.h"
+
+int main() {
+    int i;
+
+    unsigned int row;
+    unsigned int col;
+    inputRowAndCol(&row, &col);
+
+    double *twoDimenArray = (double*) malloc(sizeof(double) * row * col);
+    // 每次输入一行数据
+    for (i = 0; i < row; i++) {
+        if (1 != inputDoubleArray(twoDimenArray, i, col)) {
+            puts("Runtime Error!");
+            exit(0);
+        }
+    }
+
+    // 输出二维double数组
+    outputDoubleArray(twoDimenArray, row, col);
+    // 输出第row行平均数
+    printf("One row average : %.2lf", doubleAverage(twoDimenArray, 0, col));
+
+    free(twoDimenArray);
+    twoDimenArray = NULL;
+    return 0;
+}
