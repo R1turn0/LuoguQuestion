f0dd99000aacd0d6f9d488a392877bef069b5877
R1turn0 <714669315@qq.com>
2022-10-02 11:30:00 +0800

    Upload P5737

---
+++ P1146/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int N;
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
--- P5737/main.cpp
+++ P5737/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     int x, y, year, num = 0;
-    int leap[1000];
+    std::vector<int> leap;
     std::cin >> x >> y;
     year = x;
 
     for (int i = 0; i < (y - x) + 1; i++, year++) {
-        if (year % 4 == 0) {
-            if (year % 100 != 0 || year % 400 == 0) {
-                leap[num] = year;
-                num++;
-            }
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            leap.push_back(year);
+            num++;
         }
     }
 
     std::cout << num << std::endl;
-    for (int i = 0; i < num; i++) {
-        std::cout << leap[i] << " ";
+    for (int i : leap) {
+        std::cout << i << " ";
     }
     return 0;
 }
