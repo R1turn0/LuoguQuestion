307a7e96380edea5cf03e4d2d8589ad3f2f6eb42
R1turn0 <714669315@qq.com>
2022-11-14 00:06:31 +0800

    Update P2525/main.cpp

--- P2525/main.cpp
+++ P2525/main.cpp
@@ -3,29 +3,33 @@
 #include<bits/stdc++.h>
 
 int main() {
-    int N, order;
-    int i;
-    int temp;
-    int a[1000];
-    std::vector<int> gift;
+    int N;
+    int a[10];
 
     std::cin >> N;
-//    for (i = 0; i < N; i++) {
-//        std::cin >> order;
-//        gift.push_back(order);
-//    }
-
-    for (i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         std::cin >> a[i];
     }
 
+    // prev_permutation
+    // 变换范围 [first, last) 为来自于相对于 operator< 或 comp 的字典序的所有排列集合的上个排列。
     if (std::prev_permutation(a, a + N)) {
-        for (i = 0; i < )
+        for (int i = 0; i < N; i++) {
+            std::cout << a[i] << ' ';
+        }
     }
+    else {
+        std::cout << "ERROR";
+    }
+    std::cout << std::endl;
 
-
-//    // 冒泡排序
+//    int temp;
+//    std::vector<int> gift;
 //    for (i = 0; i < N; i++) {
+//        std::cin >> temp;
+//        gift.push_back(order);
+//    }
+//    for (i = 0; i < N; i++) { // 冒泡排序
 //        for (int j = 0; j < N - i - 1; j++) {
 //            if (gift[j] > gift[j + 1]) {
 //                temp = gift[j];
