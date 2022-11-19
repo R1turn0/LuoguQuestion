86459dc0f7fe04f6973b0d2c1a09912304459391
R1turn0 <714669315@qq.com>
2022-11-19 23:49:30 +0800

    Upload P2524

---
+++ P2524/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    int N;
+    std::string map;
+
+    std::cin >> N >> map;
+    for (int i = 0; i < map.size(); i++) {
+
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+/*
+ * 123
+ * 132
+ * 213
+ * 231
+ */
