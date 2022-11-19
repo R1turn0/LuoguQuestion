85b04fd949b44c5452f10a10b6c560c9894c8062
R1turn0 <714669315@qq.com>
2022-11-20 01:15:42 +0800

    Update P2524

--- P2524/main.cpp
+++ P2524/main.cpp
@@ -1,22 +1,28 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
+
+const int fac[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};//阶乘
+
+inline int Contor(char s[], int n) {
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+//        std::cout << ans << std::endl;
+        int smaller = 0;
+        for (int j = i + 1; j < n; j++)
+            if (s[i] > s[j])
+                smaller++;
+        ans += smaller * fac[n - i - 1];
+    }
+    return ans + 1;
+}
 
 int main() {
-    int N;
-    std::string map;
+    int n;
+    char s[108];
 
-    std::cin >> N >> map;
-    for (int i = 0; i < map.size(); i++) {
-
-    }
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cin >> n >> s;
+    std::cout << Contor(s, n);
 }
 
 /*
- * 123
- * 132
- * 213
- * 231
+ * 字典序：123 > 132 > 213 > 231 > 312 > 321
  */
