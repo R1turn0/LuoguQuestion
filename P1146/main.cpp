738c42ce97b90f19b1d53feb4975503d5684ea47
R1turn0 <714669315@qq.com>
2022-11-21 11:30:00 +0800

    Update P1146

--- P1146/main.cpp
+++ P1146/main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
+#include<cstdio>
 
 int main() {
-    int N;
-
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++)
+            putchar(48 | ~i & 1);
+        for (int j = i + 1; j <= n; j++)
+            putchar(48 | i & 1);
+        putchar(10);
+    }
     return 0;
 }
