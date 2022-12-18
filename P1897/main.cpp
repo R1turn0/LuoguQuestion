4b704c7ac9d660130ba97795cb6eea0b4bb84350
R1turn0 <714669315@qq.com>
2022-12-18 11:30:00 +0800

    Upload

--- P1897/main.cpp
+++ P1897/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
     int n, tmp;
-    std::vector<int>a;
+    int a[100001];
+    int fri = 0, nex = 0;
+    int time, sun;
 
     std::cin >> n;
     for (int i = 0; i < n; i++) {
-        std::cin >> tmp;
-        a.push_back(tmp);
+        std::cin >> a[i];
     }
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n - i - 1; j++) {
+//            if (a[j] > a[j + 1]) {
+//                tmp = a[j];
+//                a[j] = a[j + 1];
+//                a[j + 1] = tmp;
+//            }
+//        }
+//    }
 
-    std::cout << "Hello, World!" << std::endl;
+    std::sort(a, a + n);
+    sun = n;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == a[i + 1])
+            sun--;
+    }
+    time = a[n - 1] * (6 + 4) + sun * 5 + n;
+    std::cout << time;
+
     return 0;
 }
