c3edd4417df1c762a428a397b19ea64a8ee25c89
R1turn0 <714669315@qq.com>
2023-01-23 11:30:00 +0800

    Upload

--- P5710/main.cpp
+++ P5710/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+int x;
+bool a, b;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    scanf("%d", &x);
+    a = !(x & 1), b = (x > 4 && x <= 12);// a满足性质1，b满足性质2
+    printf("%d %d %d %d", a & b, a | b, (a && !b || b && !a), !a && !b);
+    // 按条件输出就好啦QWQ
 }
