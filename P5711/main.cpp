eaaa5069d7e5ff4dd9861eebc3d5b86bc8bb8a9b
R1turn0 <714669315@qq.com>
2023-01-26 11:30:00 +0800

    Upload

--- P5711/main.cpp
+++ P5711/main.cpp
@@ -5,12 +5,14 @@ int main() {
     scanf("%4hd", &year);
 
     if (year % 4 == 0) {
-        if (year % 400 == 0 && year % 100 != 0)
-            std::cout << "1";
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                std::cout << '1';
+                return 0;
+            }
+        }
     }
-    else {
-        std::cout << "0";
-    }
+    std::cout << '0';
 
     return 0;
 }
