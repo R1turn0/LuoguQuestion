89e9d306d7266bf97ca39bdef52f46ad8d9f870e
R1turn0 <714669315@qq.com>
2023-02-11 11:30:00 +0800

    Upload

---
+++ P1012/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int n, tmp;
+    int num[20];
+    int res[20];
+
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> num[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 9; j++) {
+            num[i] % 10 == ;
+        }
+    }
+
+    return 0;
+}
