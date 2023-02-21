9cf34f0b857fc45409fd870cbe720bc2b27a574a
R1turn0 <714669315@qq.com>
2023-02-21 11:30:00 +0800

    Update

--- P2907/main.cpp
+++ P2907/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, k;
+    int i = 1;
+    std::cin >> n >> k;
+    while (true) {
+        int tmp = (n - k) / 2;
+        if (tmp <= 0)
+            break;
+        n = n - tmp;
+        i++;
+    }
+    std::cout << i << std::endl;
     return 0;
 }
