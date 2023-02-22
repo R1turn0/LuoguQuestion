959a3cca54e8a7ffa65e4d25c9da8e49accfc20d
R1turn0 <714669315@qq.com>
2023-02-22 11:30:00 +0800

    Update

--- P2907/main.cpp
+++ P2907/main.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
+#include<iostream>
+
+int k;
+
+int f(int a) {        //请别吐槽这个函数名-_-||
+    if (a - k > 0 && (a - k) % 2 == 0) return f((a + k) / 2) + f((a - k) / 2);//终于散伙咯
+    else return 1;    //想分手!没门
+}
 
 int main() {
-    int n, k;
-    int i = 1;
+    int n;
     std::cin >> n >> k;
-    while (true) {
-        int tmp = (n - k) / 2;
-        if (tmp <= 0)
-            break;
-        n = n - tmp;
-        i++;
-    }
-    std::cout << i << std::endl;
+    std::cout << f(n);
     return 0;
 }
