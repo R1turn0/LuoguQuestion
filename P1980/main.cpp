682cbbec8769d4e99400692de566e64dc44ba5d1
R1turn0 <714669315@qq.com>
2023-02-26 11:30:00 +0800

    Update

--- P1980/main.cpp
+++ P1980/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, x;
+    cin >> n >> x;
+    stringstream ss;
+    for (n++; --n; ss << n);     //把1-n的值都存放到字符串流中
+    string s = ss.str();
+    cout << count(s.begin(), s.end(), x + '0') << endl;    //运用count函数直接出结果，简简单单
     return 0;
 }
