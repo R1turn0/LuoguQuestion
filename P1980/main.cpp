6e4a82049b0f0979b0636256f1fd50734544520b
R1turn0 <714669315@qq.com>
2023-02-27 18:06:26 +0800

    Update

--- P1980/main.cpp
+++ P1980/main.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 int main() {
     int n, x;
-    cin >> n >> x;
-    stringstream ss;
+    std::cin >> n >> x;
+    std::stringstream ss;
     for (n++; --n; ss << n);     //把1-n的值都存放到字符串流中
-    string s = ss.str();
-    cout << count(s.begin(), s.end(), x + '0') << endl;    //运用count函数直接出结果，简简单单
+    std::string s = ss.str();
+    std::cout << count(s.begin(), s.end(), x + '0') << std::endl;    //运用count函数直接出结果，简简单单
     return 0;
 }
