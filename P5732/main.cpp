d9d61c0db478b3e5863adc9a42c0e704a08bd55d
R1turn0 <714669315@qq.com>
2023-03-06 11:30:00 +0800

    Upload

--- P5732/main.cpp
+++ P5732/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+int a[20][20];
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        a[i][0] = a[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
