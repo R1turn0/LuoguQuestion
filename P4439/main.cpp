add17312fe8440babd850f74e8c5f47714f4cb2f
R1turn0 <714669315@qq.com>
2023-03-21 11:30:00 +0800

    Upload

--- P4439/main.cpp
+++ P4439/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, sun = 2;
+    char str1, str2;
+    std::cin >> n >> str1;
+    for (int i = 0; i < n - 1; i++) {
+        std::cin >> str2;
+        if (str1 != str2) sun++;
+        str1 = str2;
+    }
+    std::cout << sun;
     return 0;
 }
