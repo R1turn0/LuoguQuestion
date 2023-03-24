454f9fa7eb02312c048acdff33298b36a7075e45
R1turn0 <714669315@qq.com>
2023-03-24 11:30:00 +0800

    Upload

--- P6866/main.cpp
+++ P6866/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <cstdio>
+
+int n, m, ans;
+char a[100][100];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; ++i)
+        scanf("%s", a[i]);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            if (a[i][j] == '*' && (!i || a[i - 1][j] == '.') && (!j || a[i][j - 1] == '.'))
+                ++ans;
+    printf("%d", ans);
     return 0;
 }
