1cff2b1baf47333b54f6b522d81dc11d65db0e0c
R1turn0 <714669315@qq.com>
2023-04-03 11:30:00 +0800

    Upload

--- P1044/main.cpp
+++ P1044/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+    int n, sun = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        sun += i;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
