14212a70897409c8e3c9b6b7b9c029a35c09bcde
R1turn0 <714669315@qq.com>
2023-04-05 11:30:00 +0800

    Upload

--- P1030/main.cpp
+++ P1030/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cstring>
+
+void beford(std::string fir, std::string aft) {
+    if (!fir.empty()) {
+        std::cout << aft[fir.size() - 1];
+        int tree = fir.find(aft[fir.size() - 1]);
+        beford(fir.substr(0, tree), aft.substr(0, tree));
+        beford(fir.substr(tree + 1), aft.substr(tree, fir.size() - tree - 1));
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    char fir[8], aft[8];
+    std::cin >> fir >> aft;
+    beford(fir, aft);
+    std::cout << std::endl;
     return 0;
 }
