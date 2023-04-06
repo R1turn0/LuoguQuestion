e708f6fbe5f20c164e446fc279ebfe42fc6f10fa
R1turn0 <714669315@qq.com>
2023-04-06 17:04:28 +0800

    Upload

--- P1044/main.cpp
+++ P1044/main.cpp
@@ -12,3 +12,5 @@ int main()
     printf("%d", f[n]);
     return 0;
 }
+
+// https://blog.csdn.net/qq_30115697/article/details/88906534?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-88906534-blog-81105031.235%5Ev27%5Epc_relevant_recovery_v2&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-88906534-blog-81105031.235%5Ev27%5Epc_relevant_recovery_v2&utm_relevant_index=1
