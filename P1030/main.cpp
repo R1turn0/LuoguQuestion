afc21d398cb17764b7ffaef7680cf38f3c87723f
R1turn0 <714669315@qq.com>
2023-04-07 14:44:26 +0800

    Upload

--- P1030/main.cpp
+++ P1030/main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
-#include <cstring>
 
 void beford(std::string fir, std::string aft) {
     if (!fir.empty()) {
-        std::cout << aft[fir.size() - 1];
-        int tree = fir.find(aft[fir.size() - 1]);
+        char stub = aft[fir.size() - 1];
+        std::cout << stub;
+        int tree = fir.find(stub);
         beford(fir.substr(0, tree), aft.substr(0, tree));
-        beford(fir.substr(tree + 1), aft.substr(tree, fir.size() - tree - 1));
+        beford(fir.substr(tree + 1),
+               aft.substr(tree, fir.size() - tree - 1));
     }
 }
 
 int main()
 {
-    char fir[8], aft[8];
+    std::string fir, aft;
     std::cin >> fir >> aft;
     beford(fir, aft);
     std::cout << std::endl;
     return 0;
 }
+
+// FDHGIBEAC
+// FHIGDEBCA
