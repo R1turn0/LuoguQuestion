b8d7a0c17f2d3e497efadfa0ce29f8a17d38e9a9
R1turn0 <714669315@qq.com>
2023-04-10 11:30:00 +0800

    Upload

--- P2550/main.cpp
+++ P2550/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+int n;
+int win[7];
+int prize[7] = {0};
+int num = 0;
+int tmp;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cin >> n;
+    for (int &i : win)
+        std::cin >> i;
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < 7; i++) {
+            std::cin >> tmp;
+            for (int &z : win)
+                if (z == tmp)
+                    num++;
+        }
+        prize[--num]++;
+        num = 0;
+    }
+    for (int i = 6; i >= 0; i--) {
+        std::cout << prize[i] << " ";
+    }
     return 0;
 }
