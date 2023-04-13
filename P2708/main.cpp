b6aa7c28f00973bf316f7c7a7df0157a34a6ac56
R1turn0 <714669315@qq.com>
2023-04-13 09:47:34 +0800

    Upload

--- P2708/main.cpp
+++ P2708/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int sun = 0;
+    std::string str;
+    std::cin >> str;
+    for (int i = 0; i < str.size() - 1; i++)
+        if (str[i] != str[i + 1])
+            sun++;
+    if (str[str.size() - 1] == '0')
+        sun++;
+    std::cout << sun;
     return 0;
 }
