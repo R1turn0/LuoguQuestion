3ac106b08bc6f3b4d10d8f7efe3ef654f2b6627a
R1turn0 <714669315@qq.com>
2023-04-17 11:30:00 +0800

    Upload

--- P1029/main.cpp
+++ P1029/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+long long m, n, ans;
 
 int main() {
-    int x, y;
-    std::cin >> x >> y;
-
+    std::cin >> m >> n;
+    if (m == n) ans--;
+    n *= m;
+    for (long long i = 1; i <= sqrt(n); i++)
+        if (n % i == 0 && std::__gcd(i, n / i) == m) ans += 2;
+    std::cout << ans;
     return 0;
 }
+
+/*
+ * Problem:
+ * x*y=P*Q
+ * gcd(x,y)=P 最大公约数
+ * lcm(x,y)=Q 最小公倍数
+ *
+ * Tip:两个数的积等于它们最大公约数和它们最小公倍数的积
+ */
