ffa2987c971f96a3a336d0003437ad3e3299a9ea
R1turn0 <714669315@qq.com>
2023-04-21 11:30:00 +0800

    Upload

--- P4613/main.cpp
+++ P4613/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int n;
+int x[4], y[4];
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) std::cin >> x[i];
+    for (int i = 0; i < n; i++) std::cin >> y[i];
     return 0;
 }
