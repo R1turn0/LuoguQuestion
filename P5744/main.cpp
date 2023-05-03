937df9e1020b2eabbcf80b149262539815faa8ec
R1turn0 <714669315@qq.com>
2023-05-03 11:30:00 +0800

    Upload

--- P5744/main.cpp
+++ P5744/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::string name;
+    int age, res;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> name >> age >> res;
+        if (res > 500)
+            res = 600;
+        else
+            res *= 1.2;
+        std::cout << name << ' ' << age + 1 << ' ' << res << std::endl;
+    }
     return 0;
 }
