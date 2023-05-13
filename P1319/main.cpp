9a7877da8516dc8a3b7db049a51acef179d8cd96
R1turn0 <714669315@qq.com>
2023-05-13 11:30:00 +0800

    Upload

--- P1319/main.cpp
+++ P1319/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    int line = 0;
+    int num;
+    int is = 0;
+    std::cin >> n;
+    while (std::cin >> num) {
+        is = !is;
+        if (is) {
+            for (int i = 0; i < num; i++) {
+                std::cout << "0";
+                line++;
+                if (line == n) {
+                    std::cout << std::endl;
+                    line = 0;
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < num; i++) {
+                std::cout << "1";
+                line++;
+                if (line == n) {
+                    std::cout << std::endl;
+                    line = 0;
+                }
+            }
+        }
+    }
     return 0;
 }
