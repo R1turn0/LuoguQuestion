9f8f7a5df4d59ad475f7d99e3ee75cc84e40c117
R1turn0 <714669315@qq.com>
2023-05-16 11:30:00 +0800

    Upload

---
+++ StackOverflow/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Fun() {
+    int i;
+    int arr[5] = {0};
+
+    for (i = 0; i <= 5; i++) {
+        arr[i] = 0;
+        printf("Hello Word\n");
+    }
+}
+
+int main() {
+    Fun();
+    return 0;
+}
