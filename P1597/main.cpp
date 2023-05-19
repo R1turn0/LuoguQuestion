397ee959c9f4f1edd91b0a7c0d2ba3f94b64f05d
R1turn0 <714669315@qq.com>
2023-05-19 11:30:00 +0800

    Upload

--- P1597/main.cpp
+++ P1597/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <cstdio>
+
+int a[3];
+char str, num;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    while (scanf("%c:=%c;", &str, &num) == 2)//充分利用c++语言优势
+        a[str - 'a'] = num >= '0' && num <= '9' ? num - '0' : a[num - 'a']; //赋值语句简洁明了
+    printf("%d %d %d", a[0], a[1], a[2]);
 }
