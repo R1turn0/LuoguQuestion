57534d4f9ddf3aa8f5ff995e6168d11e24968616
R1turn0 <714669315@qq.com>
2023-05-24 11:30:00 +0800

    Upload

--- B2006/main.cpp
+++ B2006/main.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 
 int main() {
-    double x, a, y, b, z;
+    double x, a, y, b;
     std::cin >> x >> a >> y >> b;
     std::cout << std::fixed << std::setprecision(2) << (x * a - y * b) / (a - b) << std::endl;
     return 0;
