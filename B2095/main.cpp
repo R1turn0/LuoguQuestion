3e96d7d2a31329e6a38d2bbff9d34fcaeb810eaa
R1turn0 <714669315@qq.com>
2023-05-25 11:30:00 +0800

    Upload

---
+++ B2095/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int n;
+double num[300];
+int max, min;
+
+int main() {
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+        std::cin >> num[i];
+    for (int i = 0; i < n; i++) {
+
+    }
+    return 0;
+}
