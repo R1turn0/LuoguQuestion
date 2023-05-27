2d58d507007f87d35732e2ef8223def3059a1fea
R1turn0 <714669315@qq.com>
2023-05-27 11:30:00 +0800

    Upload

--- B2095/main.cpp
+++ B2095/main.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 int n;
-double num[300];
-int max, min;
+double a[310];
 
 int main() {
     std::cin >> n;
-    for (int i = 0; i < n; i++)
-        std::cin >> num[i];
-    for (int i = 0; i < n; i++) {
-
-    }
+    for (int i = 1; i <= n; i++)
+        std::cin >> a[i];
+    std::sort(a + 1, a + n + 1);
+    double jas = 0;//计数器
+    for (int i = 2; i < n; i++)
+        jas += a[i];
+    std::cout << std::fixed << std::setprecision(2) << jas / (n - 2) << " "
+         << std::max(fabs(a[2] - jas / (n - 2)), fabs(a[n - 1] - jas / (n - 2)));
     return 0;
 }
