37e193bba0ada17f6716d9d899ee7dcf5efbab19
R1turn0 <714669315@qq.com>
2023-06-07 11:30:00 +0800

    Upload

--- P1055/main.cpp
+++ P1055/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 
 int main() {
-    int lag, pre, num;
+    int lag, pre, num, rig;
     char id;
-    int rig;
     scanf("%1d-%3d-%5d-%1c", &lag, &pre, &num, &id);
     rig = lag * 1 + (pre / 100 % 10) * 2 + (pre / 10 % 10) * 3 + (pre % 10) * 4
             + (num / 10000 % 10) * 5 + (num / 1000 % 10) * 6 + (num / 100 % 10) * 7
             + (num / 10 % 10) * 8 + (num % 10) * 9;
-    if ((rig %= 11) == 10 && id == 'X')
+    if (((rig %= 11) == 10 && id == 'X') && rig == (id - '0'))
         std::cout << "Right";
     else {
-        if (rig == id - '0')
-            std::cout << "Right";
-        else {
-            if (rig == 10)
-                printf("%d-%d-%d-X", lag, pre, num);
-            else
-                printf("%d-%d-%d-%d", lag, pre, num, rig);
-        }
+        if (rig == 10)
+            printf("%d-%d-%d-X", lag, pre, num);
+        else
+            printf("%d-%d-%d-%d", lag, pre, num, rig);
     }
     return 0;
 }
+
+//int main() {
+//    char num[10];
+//    for (char i : num) std::cin >> i;
+//    for (int i = 1; i <= 10; i++) {
+//
+//    }
+//}
