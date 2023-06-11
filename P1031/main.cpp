b188328592dc437c85d9a9215555eb196225abb9
R1turn0 <714669315@qq.com>
2023-06-11 11:30:00 +0800

    Upload

--- P1031/main.cpp
+++ P1031/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+int num[101];
+
 int main() {
-    int n;
-    std::cout << "Hello, World!" << std::endl;
+    int n, age = 0, tmp = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> num[i];
+        age += num[i];
+    }
+    age /= n;
+    for (int i = 0; i < n; i++) {
+        num[i] -= age;
+    }
+
     return 0;
 }
+
+/*
+ *  1 -1 4 -2 -1
+ */
