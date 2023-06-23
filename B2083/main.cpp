6e01ab87b01ce7a3a35037a29e775d0917820c57
R1turn0 <714669315@qq.com>
2023-06-23 11:30:00 +0800

    Upload

--- B2083/main.cpp
+++ B2083/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    std::string c;
+    int f;
+    std::cin >> a >> b >> c >> f;
+    for (int i = 0; i < b; i++) {
+        std::cout << c;
+    }
+    std::cout << std::endl;
+    while (--a > 1) {
+        std::cout << c;
+        if (f == 0){
+            for (int i = 0; i < b - 2; i++)
+                std::cout << ' ';
+            std::cout << c << std::endl;
+        }
+        else{
+            for (int i = 0; i < b - 1; i++) {
+                std::cout << c;
+            }
+            std::cout << std::endl;
+        }
+    }
+    for (int i = 0; i < b; i++) {
+        std::cout << c;
+    }
     return 0;
 }
