121ccd4b95e09b53c626712bdcf9ab6b6134ac04
R1turn0 <714669315@qq.com>
2023-07-02 11:30:00 +0800

    Upload

--- P3156/main.cpp
+++ P3156/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m;
+    int num[100];
+    std::cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        std::cin >> num[i];
+
+    }
+
     return 0;
 }
