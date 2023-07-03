83604e42f70a6cd70225763743f668a303f25331
R1turn0 <714669315@qq.com>
2023-07-03 11:30:00 +0800

    Update

--- P3156/main.cpp
+++ P3156/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
+int num[2000000];
+int stu[2000000];
+
 int main() {
     int n, m;
-    int num[100];
+    int prb;
     std::cin >> n >> m;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         std::cin >> num[i];
-
+    while (m--) {
+        std::cin >> prb;
+        std::cout << num[--prb] << std::endl;
     }
-
     return 0;
 }
