0baba80788da8c04b7c8ce2d6415cf3fea8ae21e
R1turn0 <714669315@qq.com>
2023-07-10 11:30:00 +0800

    Upload

--- P1075/main.cpp
+++ P1075/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+    for (int i = 2; i <= n; i++) if (n % i == 0) {
+        std::cout << n / i;
+        break;
+    }
     return 0;
 }
