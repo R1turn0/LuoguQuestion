db3fef5b1028b4e3558b17ecc35fc7e87b6ee0a5
R1turn0 <714669315@qq.com>
2023-07-13 11:30:00 +0800

    Update

--- P1051/main.cpp
+++ P1051/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+struct student {
+    char name[20];
+    int age;
+    int ageClass;
+    std::string cadre;
+    std::string west;
+    int thesis;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+    student stu[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> stu[i].name >> stu[i].age >> stu[i].ageClass
+        >> stu[i].cadre >> stu[i].west >> stu[i].thesis;
+    }
     return 0;
 }
