2c0f129503418910a3816ab350feefb45547e6bf
R1turn0 <714669315@qq.com>
2023-07-20 11:30:00 +0800

    Upload

--- P1062/main.cpp
+++ P1062/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <stack>
+#include <cmath>
+
+long long k, n, ans;
+std::stack<int> S;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cin >> k >> n;
+    while (n) S.push(n & 1), n >>= 1;
+    while (!S.empty()) ans += S.top() * pow(k, S.size() - 1), S.pop();
+    std::cout << ans << std::endl;
     return 0;
 }
