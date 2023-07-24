54f01bfd2c653f6f29b26ec7a18d7cb3626b31d2
R1turn0 <714669315@qq.com>
2023-07-24 11:30:00 +0800

    Upload

--- P1028/main.cpp
+++ P1028/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include<cstdio>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n, i, f[1010];
+    f[0] = f[1] = 1;
+    scanf("%d", &n);
+    for (i = 2; i <= n; i++) {
+        if (i % 2 == 0) {
+            f[i] = f[i - 1] + f[i / 2];
+        } else {
+            f[i] = f[i - 1];
+        }
+    }
+    printf("%d\n", f[n]);
 }
