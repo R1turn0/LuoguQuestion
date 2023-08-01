ee9a811dfce18a973522ac50e843501d4a7196e0
R1turn0 <714669315@qq.com>
2023-08-01 11:30:00 +0800

    Upload

--- P1009/main.cpp
+++ P1009/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+long long fact(long long, long long);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    long long num = 0;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        num += fact(1, i);
+    }
+    std::cout << num << std::endl;
     return 0;
+}
+
+long long fact(long long num, long long n) {
+    num = num * n;
+    if (n - 1 == 0)
+        return num;
+    return fact(num, n - 1);
 }
