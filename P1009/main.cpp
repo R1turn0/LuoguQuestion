4dd3aec6cf18a35e444f9e0e6e38d25a60e3af49
R1turn0 <714669315@qq.com>
2023-08-05 11:30:00 +0800

    Update main.cpp

--- P1009/main.cpp
+++ P1009/main.cpp
@@ -1,21 +1,50 @@
-#include <iostream>
+#include<iostream>
+#include<cstdio>
 
-long long fact(long long, long long);
+using namespace std;
+int n, calculate[101] = {0}, result[101] = {0};
 
-int main() {
-    int n;
-    long long num = 0;
-    std::cin >> n;
-    for (int i = 1; i <= n; i++) {
-        num += fact(1, i);
+// 高精度乘法
+void mul(int digit) {
+    int carry = 0;  // 进位
+    for (int i = 100; i >= 0; i--) {
+        calculate[i] = calculate[i] * digit + carry;    // 位数相乘加上低阶进位
+        carry = calculate[i] / 10;  // 计算该数位上需要进位的大小
+        calculate[i] = calculate[i] % 10;   // 计算该数位上的数 [0, 10)
     }
-    std::cout << num << std::endl;
-    return 0;
 }
 
-long long fact(long long num, long long n) {
-    num = num * n;
-    if (n - 1 == 0)
-        return num;
-    return fact(num, n - 1);
+// 高精度加法
+void add() {
+    int carry = 0;  // 进位
+    for (int i = 100; i >= 0; i--) {
+        result[i] = result[i] + calculate[i] + carry;
+        carry = result[i] / 10;
+        result[i] = result[i] % 10;
+    }
+}
+
+// 输出
+void output() {
+    int w = 0;
+    // 循环过滤数组中无用的0
+    for (int i = 0; i <= 100; i++) {
+        if (result[i] != 0) {
+            w = i;
+            break;
+        }
+    }
+    for (int i = w; i <= 100; i++)
+        printf("%d", result[i]);
+}
+
+int main() {
+    scanf("%d", &n);
+    result[100] = calculate[100] = 1;
+    for (int i = 2; i <= n; i++) {
+        mul(i);
+        add();
+    }
+    output();
+    return 0;
 }
