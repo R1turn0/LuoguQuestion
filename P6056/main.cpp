f01b692cf85ba3232e5ea7275d871de0737ffd74
R1turn0 <714669315@qq.com>
2023-10-28 11:30:00 +0800

    Upload

--- P6056/main.cpp
+++ P6056/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
+int s, I, r, n;
 int main() {
-    int sus, inf, rec;
-    int N;
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    double a, b;
+    std::cin >> s >> I >> n >> a >> b;
+    for (int i = 1; i <= n; i++) {
+        int si = s, ii = I, ri = r;
+        r += ceil(ii * b);
+        I -= ceil(ii * b);
+        I += ceil(a * si * ii);
+        s -= ceil(a * si * ii);//不是floor
+        if (s < 0) {
+            I += s;
+            s = 0;
+        }
+        if (I < 0) {
+            I = 0;
+            r += I;
+        }//千万不要丢
+    }
+    std::cout << s << ' ' << I << ' ' << r;
 }
