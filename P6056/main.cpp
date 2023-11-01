efb57c4ce6082c586cd0abbbdcddbfdf85c65cb4
R1turn0 <714669315@qq.com>
2023-11-01 11:30:00 +0800

    Upload

--- P6056/main.cpp
+++ P6056/main.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
+
 int s, I, r, n;
+
 int main() {
     double a, b;
     std::cin >> s >> I >> n >> a >> b;
     for (int i = 1; i <= n; i++) {
-        int si = s, ii = I, ri = r;
+        int si = s, ii = I;
         r += ceil(ii * b);
         I -= ceil(ii * b);
         I += ceil(a * si * ii);
-        s -= ceil(a * si * ii);//不是floor
+        s -= ceil(a * si * ii);
         if (s < 0) {
             I += s;
             s = 0;
@@ -16,7 +18,7 @@ int main() {
         if (I < 0) {
             I = 0;
             r += I;
-        }//千万不要丢
+        }
     }
     std::cout << s << ' ' << I << ' ' << r;
     return 0;
