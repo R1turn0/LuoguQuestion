340688044baf9af079a3f5425cc82da8cf2e9f20
R1turn0 <714669315@qq.com>
2023-11-09 11:30:00 +0800

    Upload

--- B2108/main.cpp
+++ B2108/main.cpp
@@ -12,8 +12,11 @@ int main() {
     }
     for (int i = 1; i < n - 1; i++) {
         for (int j = 1; j < m - 1; j++) {
-            outPixel[i * m + j] = floor((pixel[i * m + j] + pixel[(i - 1) * m + j] + pixel[(i + 1) * m + j] \
-                    + pixel[i * m + (j - 1)] + pixel[i * m + (j + 1)]) * 1.0 / 5 + 0.5);
+            outPixel[i * m + j] = floor(
+                    (pixel[i * m + j] + pixel[(i - 1) * m + j] + pixel[(i + 1) * m + j] + pixel[i * m + (j - 1)] +
+                     pixel[i * m + (j + 1)]) * 1.0 / 5 + 0.5);
+            // floor 函数用于向下取整，与四舍五入不同，下取整是直接取按照数轴上最接近要求值的左边值，即不大于要求值的最大的那个整数值。
+            // 例如：3.8向下取整为3，当然也可以用 round 函数，即四舍五入
         }
     }
     for (int i = 0; i < n * m; i++) {
