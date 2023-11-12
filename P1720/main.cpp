0dd528a37e9ea15443bfdcd98873c1dcb2036ef8
R1turn0 <714669315@qq.com>
2023-11-12 11:30:00 +0800

    Upload

--- P1720/main.cpp
+++ P1720/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cmath>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int n;
+    scanf_s("%d", &n, sizeof(int));
+    printf("%.2f",(pow(((1+sqrt(5))/2),n)-pow(((1-sqrt(5))/2),n))/sqrt(5));
     return 0;
 }
