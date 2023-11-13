c654c4785d50dae050eb7ce3313bfce07237ca0a
R1turn0 <714669315@qq.com>
2023-11-13 11:30:00 +0800

    Upload

--- P1720/main.cpp
+++ P1720/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-int main()
-{
+int main() {
     int n;
     scanf_s("%d", &n, sizeof(int));
-    printf("%.2f",(pow(((1+sqrt(5))/2),n)-pow(((1-sqrt(5))/2),n))/sqrt(5));
+    printf("%.2f", (pow(((1 + sqrt(5)) / 2), n) - pow(((1 - sqrt(5)) / 2), n)) / sqrt(5));
     return 0;
 }
