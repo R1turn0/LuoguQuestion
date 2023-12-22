97bf78529d7c27afb5873652fe66976388d55f7b
R1turn0 <714669315@qq.com>
2023-12-22 11:30:00 +0800

    Upload

--- P1042/main.cpp
+++ P1042/main.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
 
-std::string fraction;
+int scorer(const std::string &fraction, int ps);
 
-//char faction[2500][25];
 
 int main() {
-//    std::cout << "输入字符串（在'E'的位置上停止读取）：" << std::endl;
-
-    // 从标准输入读取整行数据
-    std::getline(std::cin, fraction);
-
-    // 找到字符串中第一个'E'的位置
-    size_t pos = fraction.find('E');
-
-    // 如果找到了'E'，截取字符串到'E'位置
-    if (pos != std::string::npos) {
-        fraction = fraction.substr(0, pos);
+    std::string fraction;
+    while (true) {
+        static std::string input;
+        // 从标准输入读取整行数据
+        std::getline(std::cin, input);
+        // 找到字符串中第一个'E'的位置
+        size_t pos = input.find('E');
+        // 如果找到了'E'，截取字符串到'E'位置
+        if (pos != std::string::npos) {
+            input = input.substr(0, pos);
+            fraction.append(input);
+            break;
+        }
+        fraction.append(input);
     }
 
 
-    for (int i = 0; i < (fraction.size() / 11 + fraction.size() % 11 == 0 ? 0 : 1); i++) {
-        std::cout << fraction << std::endl;
-    }
 
+//    std::cout << fraction << std::endl;
+    scorer(fraction, 11);
+    std::cout << std::endl;
+    scorer(fraction, 21);
+
+    return 0;
+}
+
+int scorer(const std::string &fraction, int ps) {
+//    for (size_t i = 0; i < fraction.size(); i += ps) {
+//        std::string substr = fraction.substr(i, ps);
+//        int win_w = 0;
+//        for (char ch: substr) {
+//            if (ch == 'W') {
+//                win_w++;
+//            }
+//        }
+//        std::cout << win_w << ":" << substr.size() - win_w << std::endl;
+//    }
+
+    int w_win = 0, l_win = 0;
+    for (int i = 0; fraction[i] != fraction.empty(); i++) {
+        if (fraction[i] == 'W') {
+            w_win++;
+        } else if (fraction[i] == 'L') {
+            l_win++;
+        }
+        if ((w_win >= ps || l_win >= ps) && (w_win - l_win > 1 || w_win - l_win < -1)) {
+            std::cout << w_win << ":" << l_win << std::endl;
+            w_win = l_win = 0;
+        }
+    }
+    std::cout << w_win << ":" << l_win << std::endl;
     return 0;
 }
