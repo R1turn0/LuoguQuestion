e7a6583ecd0e9bc93ce731c2fd28c5571ec3b8f0
R1turn0 <714669315@qq.com>
2024-01-05 10:32:14 +0800

    Upload

--- P1579/main.cpp
+++ P1579/main.cpp
@@ -1,38 +1,30 @@
 #include <iostream>
 
-int main() {
-/******一维数组******/
-    short int array_1[6] = {0};
-    printf("///******一维数组******///\t");
-    printf("short int array_1[6] = {0};\t");
-    putchar('\n');
-    printf("(1)          array_1 = %8d\t\t", array_1);
-    printf("(2)      array_1 + 1 = %8d\t\t", array_1 + 1);
-    printf("(3)         &array_1 = %8d\t\t", &array_1);
-    printf("(4)     &array_1 + 1 = %8d\t\t", &array_1 + 1);
-    putchar('\n');
-    printf("(5)       array_1[0] = %8d\t\t", array_1[0]);
-    printf("(6)   array_1[0] + 1 = %8d\t\t", array_1[0] + 1);
-    printf("(7)      &array_1[0] = %8d\t\t", &array_1[0]);
-    printf("(8)  &array_1[0] + 1 = %8d\t\t", &array_1[0] + 1);
-    putchar('\n');
+bool is_prime_numbers(int num);
 
-    /******二维数组******/
-    short int array_2[2][4] = {0};
-    printf("\n///******二维数组******///\n");
-    printf("short int array_2[2][4] = {0};\n");
-    printf("(1)          array_2 = %d\n", array_2);
-    printf("(2)      array_2 + 1 = %d\n", array_2 + 1);
-    printf("(3)         &array_2 = %d\n", &array_2);
-    printf("(4)     &array_2 + 1 = %d\n", &array_2 + 1);
-    printf("(5)       array_2[0] = %d\n", array_2[0]);
-    printf("(6)   array_2[0] + 1 = %d\n", array_2[0] + 1);
-    printf("(7)      &array_2[0] = %d\n", &array_2[0]);
-    printf("(8)  &array_2[0] + 1 = %d\n", &array_2[0] + 1);
-    printf("(9)       array_2[0][0] = %d\n", array_2[0][0]);
-    printf("(10)  array_2[0][0] + 1 = %d\n", array_2[0][0] + 1);
-    printf("(11)     &array_2[0][0] = %d\n", &array_2[0][0]);
-    printf("(12) &array_2[0][0] + 1 = %d\n", &array_2[0][0] + 1);
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 2; i < n / 3; i++) {
+        if (!is_prime_numbers(i)) continue;
+        for (int j = i; j < n / 3; j++) {
+            if (!is_prime_numbers(j)) continue;
+            int k = n - i - j;
+            if (is_prime_numbers(k)) {
+                std::cout << i << " " << j << " " << k << std::endl;
+                return 0;
+            }
+        }
+    }
 
     return 0;
+}
+
+bool is_prime_numbers(int num) {
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
