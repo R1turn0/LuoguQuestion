c38e6dc361f86730e42b1280de4c75a455df423e
R1turn0 <714669315@qq.com>
2024-01-11 15:15:54 +0800

    Upload

--- P2692/main.cpp
+++ P2692/main.cpp
@@ -1,35 +1,69 @@
-#include <iostream>
+//#include <iostream>
+//
+//static int xy[5000][5000] = {0};
+//
+//int main() {
+//    int n, m, b, g;
+//    int x, y;
+//    int sum = 0;
+//    std::cin >> n >> m >> b >> g;
+//
+//    while (b--) {
+//        std::cin >> x >> y;
+//        for (int i = x - 1; i <= y - 1; i++) {
+//            for (int j = 0; j < m; j++) {
+//                xy[i][j]++;
+//            }
+//        }
+//    }
+//    while (g--) {
+//        std::cin >> x >> y;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = x - 1; j <= y - 1; j++) {
+//                xy[i][j]++;
+//            }
+//        }
+//    }
+//
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < m; j++) {
+//            if (xy[i][j] == 0) sum++;
+//        }
+//    }
+//    std::cout << n * m - sum << std::endl;
+//    return 0;
+//}
 
-static int xy[5000][5000] = {0};
+#include <iostream>
+#include <cstring>
+
+static int xy[5000] = {0};
 
 int main() {
     int n, m, b, g;
     int x, y;
-    int sum = 0;
+    int result_x = 0, result_y = 0;
     std::cin >> n >> m >> b >> g;
-
     while (b--) {
         std::cin >> x >> y;
         for (int i = x - 1; i <= y - 1; i++) {
-            for (int j = 0; j < m; j++) {
-                xy[i][j]++;
-            }
+            xy[i]++;
         }
     }
-    while (g--) {
-        std::cin >> x >> y;
-        for (int i = 0; i < n; i++) {
-            for (int j = x - 1; j <= y - 1; j++) {
-                xy[i][j]++;
-            }
-        }
+    for (int i = 0; i < n; i++) {
+        if (xy[i] == 0) result_x++;
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (xy[i][j] == 0) sum++;
+    memset(xy, 0, n * sizeof(int));
+    while (g--) {
+        std::cin >> x >> y;
+        for (int i = x - 1; i <= y - 1; i++) {
+            xy[i]++;
         }
     }
-    std::cout << n * m - sum << std::endl;
+    for (int i = 0; i < m; i++) {
+        if (xy[i] == 0) result_y++;
+    }
+    std::cout << (n * m) - (result_x * result_y) << std::endl;
     return 0;
 }
