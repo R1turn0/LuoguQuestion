63917bfc21271ca17de8b196308bf67cd783590c
R1turn0 <714669315@qq.com>
2024-03-13 11:30:00 +0800

    Upload

--- B2024/main.cpp
+++ B2024/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main() {
+    double num;
+    std::cin >> num;
+    printf("%f\n", num);
+    printf("%0.5f\n", num);
+    printf("%e\n", num);
+    printf("%g\n", num);
     return 0;
 }
