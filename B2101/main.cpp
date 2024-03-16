8b7552336449082c81f1f3862baf4816e582c103
R1turn0 <714669315@qq.com>
2024-03-16 11:30:00 +0800

    Upload

--- B2101/main.cpp
+++ B2101/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+static int m, n;
+static int temp = 0, sum = 0;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cin >> m >> n;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cin >> temp;
+            if (i == 0 || i == m - 1 || j == 0 || j == n - 1) sum += temp;
+        }
+    }
+    std::cout << sum;
     return 0;
 }
