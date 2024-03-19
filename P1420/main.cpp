d351b3638e3a4389363dc29f8970485cdde19436
R1turn0 <714669315@qq.com>
2024-03-19 11:30:00 +0800

    Upload

---
+++ P1420/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int n, temp = 0, sum = 0;
+    int a, b;
+    std::cin >> n >> a;
+    for (int i = 0; i < n - 1; i++) {
+        std::cin >> b;
+        if (b == a + 1) {
+            temp++;
+        }
+        else {
+            if (temp > sum) sum = temp;
+            temp = 0;
+        }
+        a = b;
+    }
+    if (temp > sum) sum = temp;
+    std::cout << ++sum;
+    return 0;
+}
