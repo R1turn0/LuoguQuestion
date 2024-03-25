613a902cf6cf3ec2e53fdfe8260cdacd8d60aab2
R1turn0 <714669315@qq.com>
2024-03-25 11:30:00 +0800

    Upload

---
+++ P1161/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+static int light[5000];
+double a;
+int t;
+
+int main() {
+    int n;
+    std::cin >> n;
+    while (n--) {
+        std::cin >> a >> t;
+        for (int i = 1; i <= t; i++) {
+            light[(int)(a * i)] = !light[(int)(a * i)];
+        }
+    }
+    for (int i = 1; ;i++) {
+        if (light[i] == 1) {
+            std::cout << i;
+            break;
+        }
+    }
+    return 0;
+}
