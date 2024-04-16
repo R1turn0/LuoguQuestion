f823102bd7e9d0468eb4167c72b0e3474e3b2272
R1turn0 <714669315@qq.com>
2024-04-16 15:31:44 +0800

    Upload

--- P1304/main.cpp
+++ P1304/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    std::cin >> n;
+    for (int i = 1; i <= (n - 2) / 2; i++) {
+        std::cout << 2 * i + 2 << "=" << std::endl;
+    }
     return 0;
 }
