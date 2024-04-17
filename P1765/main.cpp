3b60d09f9b89adbc8190beffab02757f3effeaed
R1turn0 <714669315@qq.com>
2024-04-17 11:30:00 +0800

    Upload

--- P1765/main.cpp
+++ P1765/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+static std::string str;
+static int num[26] = {1, 2, 3,
+                      1, 2, 3,
+                      1, 2, 3,
+                      1, 2, 3,
+                      1, 2, 3,
+                      1, 2, 3, 4,
+                      1, 2, 3,
+                      1, 2, 3, 4
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int sum = 0;
+    getline(std::cin, str);
+    for (char i: str) {
+        if (i >= 'a' && i <= 'z')
+            sum += num[i - 'a'];
+        else if (i >= 'A' && i <= 'Z')
+            sum += num[i - 'A'];
+        else if (i == ' ') sum++;
+    }
+    std::cout << sum << std::endl;
     return 0;
 }
